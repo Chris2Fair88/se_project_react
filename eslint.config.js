509fae05928c4623bf004055f5e35d14d8af6507
Chris Fairbanks <Chris2Fair88@gmail.com>
2025-03-20 17:20:12 -0400

    Configure eslint, viteconfig and package.json

--- eslint.config.js
+++ eslint.config.js
@@ -33,6 +33,13 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      "react/prop-types": 0,
     },
+
+    overrides:[
+      {
+        files:["*.js", "*.jsx"],
+      }
+    ]
   },
 ]
